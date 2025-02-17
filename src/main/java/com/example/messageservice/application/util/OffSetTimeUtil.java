3f14c140021301bbb87aa7764d7daacedb7fbf10
Sergio Ciufo <carminesergio.ciufo@gmail.com>
2025-02-17 12:49:41 +0100

    minor fix

--- src/main/java/com/example/messageservice/application/util/OffSetTimeUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.messageservice.application.util;
-
-import java.time.OffsetDateTime;
-
-public class OffSetTimeUtil {
-    public OffsetDateTime stringToOffsetDateTime(String dateString) {
-        return dateString != null ? OffsetDateTime.parse(dateString) : null;
-    }
-}
